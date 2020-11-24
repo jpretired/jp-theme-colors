9ffc51f566c504242d72979fd35ca68b02417c71
jpretired <josef.prause@gmail.com>
2020-11-24 17:18:53 +0100

    Initial commit

---
+++ module.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * JpColors-theme.  Here we are extending an existing theme.
+ * Instead, you could extend AbstractModule and implement ModuleThemeInterface directly.
+ */
+/* 
+ * Copyright (C) 2020 josef
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace JpNamespace;
+
+use Fisharebest\Webtrees\Module\ColorsTheme;
+use Fisharebest\Webtrees\Module\ModuleCustomInterface;
+use Fisharebest\Webtrees\Module\ModuleCustomTrait;
+use Fisharebest\Webtrees\View;
+
+return new class extends ColorsTheme implements ModuleCustomInterface {
+    use ModuleCustomTrait;
+
+    /**
+     * @return string
+     */
+    public function title(): string
+    {
+        return 'JpColors-barvy';
+    }
+
+    /**
+     * Bootstrap the module
+     */
+    public function boot(): void
+    {
+    }
+
+    /**
+     * Where does this module store its resources
+     *
+     * @return string
+     */
+    public function resourcesFolder(): string
+    {
+        return __DIR__ . '/resources/';
+    }
+
+    /**
+     * Add our own stylesheet to the existing stylesheets.
+     *
+     * @return array<string>
+     */
+    public function stylesheets(): array
+    {
+        $stylesheets = parent::stylesheets();
+
+        // NOTE - a future version of webtrees will allow the modules to be stored in a private folder.
+        // Only files in the /public/ folder will be accessible via the webserver.
+        // Since modules cannot copy their files to the /public/ folder, they need to provide them via a callback.
+        $stylesheets[] = $this->assetUrl('css/theme.css');
+
+        return $stylesheets;
+    }
+};
